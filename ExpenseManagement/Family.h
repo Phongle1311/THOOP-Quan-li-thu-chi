7fc01c0334dbdca112aae6fbcc0313e91996d68a
Hoai Phong <76105787+Phongle1311@users.noreply.github.com>
2022-05-23 21:29:45 +0700

    Update Family.h

--- ExpenseManagement/Family.h
+++ ExpenseManagement/Family.h
@@ -1,17 +1,28 @@
 #pragma once
 #include "MyException.h"
+
+// Thu chi hằng tháng
 class Family
 {
 private:
-	double salary;
+	// Thu nhập gồm: lương của vợ/chồng, thu nhập chung, thu nhập khác
+	double wife_salary;
+	double husband_salary;
+	double joint_income;
 	double other_income;
+
+	// Chi phí gồm: chi phí sinh hoạt, chi phí phát sinh
 	double expense;
 	double costs_incurred;
+
 public:
 	Family();
-	Family(double salary, double other_income, double expense, double costs_incurred);
+	Family(double wife_salary, double husband_salary, double joint_income, double other_income, double expense, double costs_incurred);
+	double GetTotalIncome();		// Tổng thu nhập
+	double GetTotalCost();			// Tổng chi phí
 	double GetDeposit();
-	double GetRemainingMoney();
-	void Input();
+	double GetRemainingMoney();		// Phần tiền còn dư
+	void IncomeInput();				// Nhập thu riêng
+	void CostInput();				// Nhập chi riêng
+	void Input();					// Nhập thu chi chung
 };
-
