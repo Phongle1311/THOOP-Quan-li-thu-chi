af48cfa97b57c4a5bc799d9fc92fcb27bca83b95
Hoai Phong <76105787+Phongle1311@users.noreply.github.com>
2022-05-25 11:45:48 +0700

    Update SavingsAccount.cpp
    Sửa một số lỗi

--- ExpenseManagement/SavingsAccount.cpp
+++ ExpenseManagement/SavingsAccount.cpp
@@ -1,4 +1,4 @@
-﻿#include "SavingsAccount.h"
+#include "SavingsAccount.h"
 SavingsAccount::SavingsAccount() {
 	total_balance = 0;
 }
@@ -12,20 +12,20 @@ bool SavingsAccount::AddSavingsBook(SavingsBook book, double money) {
 	book.MakeDeposit(money);
 	book.TermInput();
 	book.InterestInput();
-	book.SetIssuedDate(Date.current());
+	book.SetIssuedDate(Date::Current());
 	book_list.push_back(book);
-	total_balance += money;		// Lúc này chưa có lãi, nên chỉ cộng thêm tiền gốc 
+	total_balance += money;									// Lúc này chưa có lãi, nên chỉ cộng thêm tiền gốc 
 	return true;
 }
 bool SavingsAccount::RemoveSavingsBook() {
 	int count = 0;
 	for (int i = 0; i < book_list.size(); i++) {
 		if (book_list[i].IsDueDate()) {
-			total_balance += book_list[i].GetInterest(); // Lấy ra lãi của những sổ đã hết kỳ hạn
-			book_list.erase(i);							// Xóa sổ tiết kiệm
+			total_balance += book_list[i].GetInterest();	// Lấy ra lãi của những sổ đã hết kỳ hạn
+			book_list.erase(book_list.begin() + i);			// Xóa sổ tiết kiệm
 			count++;
 			i--;
 		}
 	}
-	return count == 0 ? false : true;					//Kiểm tra tháng này có sổ nào hết kỳ hạn không
+	return count == 0 ? false : true;						//Kiểm tra tháng này có sổ nào hết kỳ hạn không
 }
