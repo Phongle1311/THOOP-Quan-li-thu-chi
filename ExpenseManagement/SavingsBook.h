1453c6904826c133f765a6343e2979d0eeb3adb6
Hoai Phong <76105787+Phongle1311@users.noreply.github.com>
2022-05-25 11:47:50 +0700

    Update SavingsBook.h
    1 vài thay đổi nhỏ (bỏ const & ở setisssue)

--- ExpenseManagement/SavingsBook.h
+++ ExpenseManagement/SavingsBook.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #include <string>
 #include <iostream>
 #include "Date.h"
@@ -7,19 +7,18 @@ using namespace std;
 class SavingsBook
 {
 private:
-	double balance;
-	int term;
-	double interest_rate;
-	Date issued_date;
+	double balance;									// Số dư
+	int term;										// Kì hạn
+	double interest_rate;							// Lãi suất
+	Date issued_date;								// Ngày phát hành
 public:
 	SavingsBook();
 	SavingsBook(double balance, int term, double interest_rate, Date issued_date);
-	double GetBalance();							//Lấy số dư 
-	double GetInterest();							//Lấy tiền lãi
-	bool IsDueDate();								//Kiểm tra đến kỳ hạn chưa
-	bool MakeDeposit(double money);					//Gửi tiền vào sổ
-	void TermInput();								//Nhập kỳ hạn
-	void InterestInput();							//Nhập lãi suất
-	void SetIssuedDate(const Date& issued_date);	//Đặt ngày phát hành sổ
+	double GetBalance();							// Lấy số dư 
+	double GetInterest();							// Lấy tiền lãi
+	bool IsDueDate();								// Kiểm tra đến kỳ hạn chưa
+	bool MakeDeposit(double money);					// Gửi tiền vào sổ
+	void TermInput();								// Nhập kỳ hạn
+	void InterestInput();							// Nhập lãi suất
+	void SetIssuedDate(Date issued_date);			// Đặt ngày phát hành sổ
 };
-
