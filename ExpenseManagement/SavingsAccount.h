e363f389a52e657c5e047cf5182af3cac121d1e7
Hoai Phong <76105787+Phongle1311@users.noreply.github.com>
2022-05-25 11:45:15 +0700

    Update SavingsAccount.h
    nothing

--- ExpenseManagement/SavingsAccount.h
+++ ExpenseManagement/SavingsAccount.h
@@ -10,8 +10,8 @@ private:
 public:
 	SavingsAccount();
 	~SavingsAccount();
-	double GetTotalBalance();								//Lấy tổng tiền hiện có trong tất cả các sổ
-	bool AddSavingsBook(SavingsBook book, double money);	//Mở thêm 1 sổ
-	bool RemoveSavingsBook();								//Xóa những sổ đã hết kỳ hạn
+	double GetTotalBalance();								// Lấy tổng tiền hiện có trong tất cả các sổ
+	bool AddSavingsBook(SavingsBook book, double money);					// Mở thêm 1 sổ
+	bool RemoveSavingsBook();								// Xóa những sổ đã hết kỳ hạn
 };
 
