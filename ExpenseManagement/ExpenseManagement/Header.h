8b22192a4a65018deca3e1080cb9e8fe196d2c23
Hoai Phong <76105787+Phongle1311@users.noreply.github.com>
2022-05-31 22:24:16 +0700

    Update Header.h
    thêm hàm đọc database

--- ExpenseManagement/ExpenseManagement/Header.h
+++ ExpenseManagement/ExpenseManagement/Header.h
@@ -1,6 +1,12 @@
 #pragma once
 #include <iostream>
 #include <conio.h>
+#include "Date.h"
+#include "Family.h"
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <vector>
 using namespace std;
 
 bool nhapSoNguyen(int& n);
@@ -9,3 +15,7 @@ void Menu1();
 void Menu2();
 void Menu3();
 void Menu4();
+void readDatabase(const char* fileName);
+void writeOneMonthFile(const char* fileName, int id);
+void writeAllMonthFile(const char* fileName); 
+void writeDatabase(const char* fileName);
